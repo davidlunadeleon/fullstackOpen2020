072639ac46ccd1f19e012cf8bbaacc532ea84274
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-12 22:23:10 -0500

    Add exercise 1.4 in part1

--- part1/courseinformation/src/index.js
+++ part1/courseinformation/src/index.js
@@ -38,7 +38,7 @@ const Content = (props) => {
 const Total = (props) => {
 	let total = 0;
 	props.exercises.forEach((element) => {
-		total += element;
+		total += element.exercises;
 	});
 	return (
 		<div>
@@ -49,26 +49,26 @@ const Total = (props) => {
 
 const App = () => {
 	const course = 'Half Stack application development';
-	const part1 = {
-		name: 'Fundamentals of React',
-		exercises: 10
-	};
-	const part2 = {
-		name: 'Using props to pass data',
-		exercises: 7
-	};
-	const part3 = {
-		name: 'State of a component',
-		exercises: 14
-	};
+	const parts = [
+		{
+			name: 'Fundamentals of React',
+			exercises: 10
+		},
+		{
+			name: 'Using props to pass data',
+			exercises: 7
+		},
+		{
+			name: 'State of a component',
+			exercises: 14
+		}
+	];
 
 	return (
 		<div>
 			<Header course={course} />
-			<Content parts={[part1, part2, part3]} />
-			<Total
-				exercises={[part1.exercises, part2.exercises, part3.exercises]}
-			/>
+			<Content parts={parts} />
+			<Total exercises={parts} />
 		</div>
 	);
 };
