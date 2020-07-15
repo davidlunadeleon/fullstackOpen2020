ecce4593bfdedab58295f30f0ff0bbccc03f1c56
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-14 19:49:52 -0500

    Add exercise 1.7 in part1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,10 +12,10 @@ const Button = ({ text, handleClick, value }) => {
 	return <button onClick={func}>{text}</button>;
 };
 
-const DisplayStatistic = ({ text, numReviews }) => {
+const DisplayStatistic = ({ text, num }) => {
 	return (
 		<div>
-			{text} {numReviews}
+			{text} {num}
 		</div>
 	);
 };
@@ -26,6 +26,24 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
+	const getPercentage = () => {
+		let ans = good / (good + neutral + bad);
+		if (isNaN(ans)) {
+			return '0 %';
+		} else {
+			return `${ans * 100} %`;
+		}
+	};
+
+	const getAverage = () => {
+		let ans = (good - bad) / (good + neutral + bad);
+		if (isNaN(ans)) {
+			return 0;
+		} else {
+			return ans;
+		}
+	};
+
 	return (
 		<div>
 			<DisplayTitle text="give feedback" />
@@ -33,9 +51,12 @@ const App = () => {
 			<Button text="neutral" handleClick={setNeutral} value={neutral} />
 			<Button text="bad" handleClick={setBad} value={bad} />
 			<DisplayTitle text="statistic" />
-			<DisplayStatistic text="good" numReviews={good} />
-			<DisplayStatistic text="neutral" numReviews={neutral} />
-			<DisplayStatistic text="bad" numReviews={bad} />
+			<DisplayStatistic text="good" num={good} />
+			<DisplayStatistic text="neutral" num={neutral} />
+			<DisplayStatistic text="bad" num={bad} />
+			<DisplayStatistic text="all" num={good + neutral + bad} />
+			<DisplayStatistic text="average" num={getAverage()} />
+			<DisplayStatistic text="positive" num={getPercentage()} />
 		</div>
 	);
 };
