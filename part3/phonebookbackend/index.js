18031a059df8c6e0b7d15df8868ccaa62038da5f
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-22 17:09:14 -0500

    Add exercise 3.5 in part 3

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
 	{ name: 'Arto Hellas', number: '040-123456', id: 1 },
 	{ name: 'Ada Lovelace', number: '30-123456', id: 2 },
 	{ name: 'Dan Abramov', number: '12-345678', id: 3 },
 	{ name: 'Pepe Leal', number: '12-901234', id: 4 }
 ];
+
+const makeId = () => {
+	let id = 0;
+	do {
+		id = Math.floor(Math.random() * 1000);
+	} while (persons.includes((p) => p.id === id));
+	return id;
+};
 
 app.route('/').get((req, res) => {
 	res.send('<h1>Hello world!</h1>');
@@ -19,9 +29,25 @@ app.route('/info').get((req, res) => {
 	);
 });
 
-app.route('/api/persons').get((req, res) => {
-	res.json(persons);
-});
+app.route('/api/persons')
+	.get((req, res) => {
+		res.json(persons);
+	})
+	.post((req, res) => {
+		if (!req.body.name || !req.body.phone) {
+			return res.status(400).json({
+				error: 'Fields missing!'
+			});
+		}
+		const id = makeId();
+		const newPerson = {
+			name: req.body.name,
+			phone: req.body.phone,
+			id: id
+		};
+		persons = persons.concat(newPerson);
+		res.json(newPerson);
+	});
 
 app.route('/api/persons/:id')
 	.get((req, res) => {
