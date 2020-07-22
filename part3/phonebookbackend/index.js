cb193befc0ea4d921601a7930465b8c69fd1594c
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-21 21:48:28 -0500

    Create express server for 3.1 phonebook - Exercise 3.1 done - Add gitignores for certain directories

---
+++ part3/phonebookbackend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+	{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+	{ name: 'Ada Lovelace', number: '30-123456', id: 2 },
+	{ name: 'Dan Abramov', number: '12-345678', id: 3 },
+	{ name: 'Pepe Leal', number: '12-901234', id: 4 }
+];
+
+app.route('/').get((req, res) => {
+	res.send('<h1>Hello world!</h1>');
+});
+
+app.route('/api/persons').get((req, res) => {
+	res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
