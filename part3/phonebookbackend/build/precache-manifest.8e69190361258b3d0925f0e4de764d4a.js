95dc1bdd66e532b462bf86631f147d38b0f19fb7
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-22 18:44:22 -0500

    Add exercises 3.10 and 3.11
    - uibuild

---
+++ part3/phonebookbackend/build/precache-manifest.8e69190361258b3d0925f0e4de764d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278c2a7f06d82b2f61d0d4f5101e274c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68714323783714595a0a",
+    "url": "/static/css/main.2be1f899.chunk.css"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68714323783714595a0a",
+    "url": "/static/js/main.6fcb6d55.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
