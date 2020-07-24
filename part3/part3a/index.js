5ac3435faa1cb19ece5a8bb7445eb86ca7477898
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-24 17:59:58 -0500

    Update notes backend

--- part3/part3a/index.js
+++ part3/part3a/index.js
@@ -32,32 +32,6 @@ app.use(
 	})
 );
 
-let notes = [
-	{
-		id: 1,
-		content: 'HTML is easy',
-		date: '2019-05-30T17:30:31.098Z',
-		important: true
-	},
-	{
-		id: 2,
-		content: 'Browser can execute only Javascript',
-		date: '2019-05-30T18:39:34.091Z',
-		important: false
-	},
-	{
-		id: 3,
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		date: '2019-05-30T19:20:14.298Z',
-		important: true
-	}
-];
-
-const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
-};
-
 app.route('/').get((req, res) => {
 	res.send('<h1>Hello, world!</h1>');
 });
@@ -68,19 +42,18 @@ app.route('/api/notes')
 			res.json(notes);
 		});
 	})
-	.post((req, res) => {
+	.post((req, res, next) => {
 		const body = req.body;
-		if (!body.content) {
-			return res.status(400).json({ error: 'content missing' });
-		}
 		const note = new Note({
 			content: body.content,
 			important: body.important || false,
 			date: new Date()
 		});
-		note.save().then((savedNote) => {
-			res.json(savedNote);
-		});
+		note.save()
+			.then((savedNote) => {
+				res.json(savedNote);
+			})
+			.catch((error) => next(error));
 	});
 
 app.route('/api/notes/:id')
@@ -127,6 +100,8 @@ const errorHandler = (error, req, res, next) => {
 	console.log(error);
 	if (error.name === 'CastError') {
 		return res.status(400).send({ error: 'Malformated id' });
+	} else if (error.name === 'ValidationError') {
+		return res.status(400).json({ error: error.message });
 	}
 	next(error);
 };
