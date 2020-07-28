65bcb86d0728209e88b18c15ca14efc6856897d7
davidlunadeleon <davidlunadeleon@gmail.com>
2020-07-28 17:24:15 -0500

    Add exercise 4.15
    - Add route to create and see users
    - Add rest files to create and get all users

--- part4/bloglistbackend/controllers/user.js
+++ part4/bloglistbackend/controllers/user.js
@@ -3,20 +3,26 @@ const userRouter = require('express').Router();
 
 const User = require('../models/user');
 
-userRouter.route('/').post(async (req, res) => {
-	const body = req.body;
+userRouter
+	.route('/')
+	.post(async (req, res) => {
+		const body = req.body;
 
-	const saltRounds = 10;
-	const passwordHash = await bcrypt.hash(body.password, saltRounds);
+		const saltRounds = 10;
+		const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-	const user = new User({
-		username: body.username,
-		name: body.name,
-		passwordHash
+		const user = new User({
+			username: body.username,
+			name: body.name,
+			passwordHash
+		});
+
+		const savedUser = await user.save();
+		res.json(savedUser);
+	})
+	.get(async (req, res) => {
+		const users = await User.find({});
+		res.json(users);
 	});
-
-	const savedUser = await user.save();
-	res.json(savedUser);
-});
 
 module.exports = userRouter;
