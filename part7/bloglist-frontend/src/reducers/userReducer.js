e318a007795c9f985efcba038adcfa4bbc6db4e5
davidlunadeleon <davidlunadeleon@gmail.com>
2020-08-18 16:18:41 -0500

    Create userReducer to handle user stuff
    - Handle login
    - Handle logout
    - Handle restoring previous session

---
+++ part7/bloglist-frontend/src/reducers/userReducer.js
@@ -0,0 +1,52 @@
+import loginService from '../services/login';
+import blogsService from '../services/blogs';
+
+const userReducer = (state = null, action) => {
+	switch (action.type) {
+		case 'LOGIN':
+			return action.data;
+		case 'LOGOUT':
+			return null;
+		default:
+			break;
+	}
+	return state;
+};
+
+export const loginUser = (username, password) => {
+	return async (dispatch) => {
+		const user = await loginService.login({ username, password });
+		window.localStorage.setItem('loggedBlogUser', JSON.stringify(user));
+		blogsService.setToken(user.token);
+		dispatch({
+			type: 'LOGIN',
+			data: { user }
+		});
+	};
+};
+
+export const logoutUser = () => {
+	return async (dispatch) => {
+		window.localStorage.removeItem('loggedBlogUser');
+		dispatch({
+			type: 'LOGOUT'
+		});
+	};
+};
+
+export const loginPreviousSession = () => {
+	return async (dispatch) => {
+		const loggedBlogUser = window.localStorage.getItem('loggedBlogUser');
+		if (loggedBlogUser) {
+			const savedUser = JSON.parse(loggedBlogUser);
+			dispatch({
+				type: 'LOGIN',
+				data: { savedUser }
+			});
+		} else {
+			dispatch({ type: 'LOGOUT' });
+		}
+	};
+};
+
+export default userReducer;
